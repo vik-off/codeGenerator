ff0ad16595d7058f5d0537f1584ea2b77f3a82d2
Юрий Новиков <yurijnovikov@gmail.com>
2011-11-24 10:07:55 +0200

    config

---
+++ templates/vik-off-cmf-modular/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+	'name' => '__MODULE__',
+	'title' => '__MODULE_TITLE__',
+	'controller' => '__CONTROLLERNAME__',
+	'adminController' => '__ADM_CONTROLLERNAME__',
+	'resources' => array(
+		'view' => 'Просмотр',
+		'edit' => 'Редактирование',
+	),
+);
+
+?>
